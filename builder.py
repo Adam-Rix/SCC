c14ef212049d7a790d10110d7682394e243651e8
adam <valentine.bellius@gmail.com>
2025-03-30 20:13:43 +0300

    builder.py:'Erased excess version marker'

--- builder.py
+++ builder.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#vers 0.0.1
 class Ratt:
     def __init__(self,  driver):
         self.driver = driver
